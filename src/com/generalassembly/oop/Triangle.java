f1945bb75215c854bead6c60127891cdf9ff716d
Zahralsayed <zalmatooq31@gmail.com>
2025-11-23 00:26:53 +0300

    homework 05 done *

--- src/com/generalassembly/oop/Triangle.java
+++ src/com/generalassembly/oop/Triangle.java
@@ -5,7 +5,6 @@ public class Triangle extends Shape {
     private double side2;
     private double side3;
 
-
     public Triangle(double side1, double side2, double side3) {
         this.side1 = side1;
         this.side2 = side2;
